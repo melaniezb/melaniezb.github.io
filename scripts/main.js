f076eb8e4bc072ac52c1edb939de8db4e1c6d2de
Melanie <43189257+melaniezb@users.noreply.github.com>
2019-03-05 18:51:12 -0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+var myHeading = document.querySelector('h1');
+// Grab reference to the heading and store it in a variable
+// This is very similar to what we did using CSS selectors.
+// When wanting to do something to an element, you first need to select it.
+myHeading.textContent = 'Hello, world!';
+// Set the value of the myHeading variable's textContent property
+// (which represents the content of the heading) to "Hello world!".
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  var mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/honey-muffin-2.jpg') {
+    myImage.setAttribute('src', 'images/muffin-top.jpg');
+  }
+  if (mySrc === 'images/muffin-top.jpg') {
+    myImage.setAttribute('src', 'images/honey-muffin-2.jpg');
+  }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+myButton.onclick = function() {
+  setUserName();
+}
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  myHeading.innerHTML = 'Welcome back, ' + localStorage.getItem('name');
+}
+function setUserName() {
+  var myName = prompt('Please enter your name: ');
+  // prompt() brings up a dialog box, a bit like alert()
+  localStorage.setItem('name', myName);
+  // localStorage() stores data in the browser and later retrieve it
+  myHeading.innerHTML = 'Welcome to our page, ' + myName;
+}
